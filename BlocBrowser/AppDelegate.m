9fa27cb49d9ae3233b761958396d7510c0939b3f
Paul Lozada <paul_lozada@icloud.com>
2015-03-18 01:18:18 -0600

    Functional web browser

--- BlocBrowser/AppDelegate.m
+++ BlocBrowser/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "BLCViewController.h"
+
 
 @interface AppDelegate ()
 
@@ -16,7 +18,17 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    self.window = [[UIWindow alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
+    
     // Override point for customization after application launch.
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    self.window.rootViewController = [[UINavigationController alloc]initWithRootViewController:[[BLCViewController alloc]init]];
+    
+    
     return YES;
 }
 
--- BlocBrowser/BLCViewController.h
+++ BlocBrowser/BLCViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  BLCViewController.h
 //  BlocBrowser
 //
 //  Created by Paul Lozada on 2015-03-16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface BLCViewController : UIViewController
 
 @end
-
